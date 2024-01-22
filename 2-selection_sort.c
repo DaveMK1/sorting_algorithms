31dffa76e48188c46c5c2844d954c58a173befff
David Agbesinyale <davidm7040@gmail.com>
2024-01-22 07:35:07 -0800

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm
+ * @array: Array
+ * @size: array size
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		size_t min_index = i;
+
+		for (size_t j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+
+		if (i != min_index)
+		{
+			swap_int(array + i, array + min_index);
+			print_array(array, size);
+		}
+	}
+}
+
+/**
+ * swap_int - Swap two integers
+ * @a: First integer to swap
+ * @b: Second integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
