f676e06a8108fe35a558086a502c3ad9f7c9b70f
David Agbesinyale <davidm7040@gmail.com>
2024-01-23 08:45:24 -0800

    Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -34,7 +34,6 @@ listint_t *swap_node(listint_t *node, listint_t **list)
 void cocktail_sort_list(listint_t **list)
 {
 	int swapped = 1;
-
 	listint_t *current;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
@@ -53,7 +52,6 @@ void cocktail_sort_list(listint_t **list)
 				swapped = 1;
 				print_list(*list);
 			}
-
 			else
 				current = current->next;
 		}
