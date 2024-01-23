f3b1966c00e9c85df4e931687a18e4a053401980
David Agbesinyale <davidm7040@gmail.com>
2024-01-23 06:01:47 -0800

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,83 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two integers
+ * @i: First integer to swap
+ * @j: Second integer to swap
+ */
+void swap(int *i, int *j)
+{
+	int tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+
+/**
+ * lomuto_partition - Lomuto partition scheme for quicksort
+ * @array: array
+ * @low: starting index
+ * @high: ending index
+ * @size: array size
+ *
+ * Return: The position of the final element sorted
+ */
+int lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size)
+{
+	int pivot = array[high];
+	ssize_t i = low - 1;
+	ssize_t j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
+/**
+ * quicksort_imp - Implement the quicksort algorithm recursively
+ * @array: array
+ * @low: starting index
+ * @high: ending index
+ * @size: array size
+ */
+void quicksort_imp(int *array, ssize_t low, ssize_t high, size_t size)
+{
+	if (low < high)
+	{
+		int partition = lomuto_partition(array, low, high, size);
+
+		quicksort_imp(array, low, partition - 1, size);
+		quicksort_imp(array, partition + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sort an array of integers using the quicksort algorithm
+ * @array: array
+ * @size: array size
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort_imp(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -25,5 +26,7 @@ void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quicksort_imp(int *array, ssize_t low, ssize_t high, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
