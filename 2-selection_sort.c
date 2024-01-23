ce9b3843e5428c9e96ace7208b19dcbaba6314de
David Agbesinyale <davidm7040@gmail.com>
2024-01-23 03:42:42 -0800

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,11 +12,13 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (size_t i = 0; i < size - 1; i++)
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		size_t min_index = i;
 
-		for (size_t j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
